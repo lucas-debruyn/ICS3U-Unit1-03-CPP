9daf4da5ae9e6afc6c7a64fba5b2bfe38bcd5dfd
Lucas DeBruyn <lucas.debruyn@mths.ca>
2022-09-16 15:01:59 +0000

    created file

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Lucas DeBruyn All rights reserved
+//
+// Created by: Lucas DeBruyn
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.";
+}
